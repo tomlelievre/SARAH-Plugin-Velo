73895b49a4eb588fb211190a29e57b33288bece9
Thomas L <tom.lelievre@hotmail.fr>
2015-07-11 20:42:55 +0200

    Added actions (update velov station list action, common action, address action)

--- velov.js
+++ velov.js
@@ -9,7 +9,8 @@
 
 var request = require('request'),
     jsonfile = require('jsonfile'),
-    address = '145+avenue+Lacassagne,+69903+LYON';
+    properties,
+    data;
 
 const VELOV_URL = "https://api.jcdecaux.com/vls/v1/stations/";
 const GEOCODING_URL = "https://maps.googleapis.com/maps/api/geocode/json";
@@ -17,198 +18,249 @@ const VELOV_LIST_FILENAME = 'velov-stations-list.json';
 
 exports.action = function (data, callback, config, SARAH) {
 
-    config = config.modules.velov;
+    properties = config.modules.velov;
 
-    if (!config.city || !config.jcDecauxApiKey || !config.googleApiKey) {
-        callback({'tts': "Désolé, ma configuration ne me permet pas de trouver l'adresse demandée."});
-        return;
+    if (!properties.city || !properties.jcDecauxApiKey || !properties.googleApiKey)
+        return callback({'tts': "Désolé, ma configuration ne me permet pas d'utiliser convenablement ce pleuguine."});
+
+    var address = '';
+
+    switch(data.action) {
+        case 'address':
+            var search = data.dictation,
+                rgxp = /Sarah donne-moi la station Velo'v la plus proche de l'adresse (.+)/i,
+                match;
+
+            // Checks Google understanding with the regexs
+            if (match = checkGoogleUnderstanding(search, rgxp))
+                address = formatAddress(match[1]);
+            else
+                return callback({'tts': "Je ne comprends pas."});
+
+            getVelovStationDataByAddress(address, callback);
+            break;
+        case 'update_velov_file':
+            updateVelovStationList(callback);
+            break;
+        default:
+        case 'common':
+            if (!properties.address)
+                return callback({'tts': "Veuillez renseigner une adresse dans la configuration du plugin vélove."});
+
+            address = formatAddress(properties.address)
+            getVelovStationDataByAddress(address, callback);
+            break;
     }
+};
 
-    var search = data.dictation;
-    var rgxp = /Sarah donne-moi les stations Velo'v les plus proches de l'adresse (.+)/i;
+var getVelovStationDataByAddress = function(address, callback) {
+        var geocodingUrl = GEOCODING_URL + '?address=' + address + ',+FR&key=' + properties.googleApiKey;
 
-    // on s'assure que Google a bien compris
-    var match = search.match(rgxp);
-    if (!match || match.length <= 1){
-        callback({'tts': "Je ne comprends pas"});
-        return;
-    }
+        // Calls the Google Geocoding API to retrieve the geocoded latitude/longitude value of the address
+        callAPI(geocodingUrl, function (geocodingApiResult) {
 
-    // on peut maintenant s'occuper des mots qui sont recherchés
-    address = formatAddress(match[1]);
+            // Gets the geocoded latitude/longitude value of the address
+            if (typeof geocodingApiResult.results[0] === 'undefined')
+                return callback({'tts': "Je n'arrive pas à trouver l'adresse."});
 
-    var geocodingUrl = GEOCODING_URL + '?address=' + address + ',+FR&key=' + config.googleApiKey;
+            var geocodingPosition = geocodingApiResult.results[0].geometry.location;
 
-    // Calls the Google Geocoding API to retrieve the geocoded latitude/longitude value of the address
-    callAPI(geocodingUrl, function (geocodingApiResult) {
+            // Retrieves the Velov stations list from JSON file
+            jsonfile.readFile(__dirname + '\\' + VELOV_LIST_FILENAME, function (error, velovApiResult) {
 
-        // Gets the geocoded latitude/longitude value of the address
-        if (typeof geocodingApiResult.results[0] === 'undefined') {
-            callback({'tts': "Je n'arrive pas à trouver l'adresse."});
-            return;
-        }
+                if (error)
+                    return callback({'tts': "Impossible de lire la liste des stations Vélove."});
 
-        var geocodingPosition = geocodingApiResult.results[0].geometry.location;
+                // Calculation of the distance between Velov station and address geocoded latitude/longitude values
+                var stationDistances = computeGeopositionDistances(velovApiResult, geocodingPosition);
 
-        // Retrieves the Velov stations list from JSON file
-        jsonfile.readFile(__dirname + '\\' + VELOV_LIST_FILENAME, function (error, velovApiResult) {
-
-            if (error) {
-                callback({'tts': "Impossible de lire la liste des stations Vélov."});
-                return;
-            }
-
-            // Calculation of the distance between Velov station and address geocoded latitude/longitude values
-            var stationDistances = computeGeopositionDistances(velovApiResult, geocodingPosition);
-
-            // Searching of nearest Velov stations compared to the address (min distance)
-            aggregateVelovApiData(findNearestVelovStations(stationDistances, 3), 0, [], function (velovApiData) {
-                var velovDataSpeech = convertVelovApiDataInSARAHSpeech(velovApiData);
-                console.log(velovDataSpeech);
-                callback({'tts': velovDataSpeech});
+                // Searching of nearest Velov stations compared to the address (min distance)
+                aggregateVelovApiData(findNearestVelovStations(stationDistances, 1), 0, [], function (velovApiData) {
+                    var velovDataSpeech = convertVelovApiDataInSARAHSpeech(velovApiData);
+                    callback({'tts': velovDataSpeech});
+                });
             });
         });
-    });
-};
+    },
 
-/**
- * @param velovApiData
- * @returns {string}
- */
-var convertVelovApiDataInSARAHSpeech = function (velovApiData) {
-    var speech = '';
+    /**
+     * @param velovApiData
+     * @returns {string}
+     */
+    convertVelovApiDataInSARAHSpeech = function (velovApiData) {
+        var speech = '';
 
-    for (var i in velovApiData) {
-        var velovData = velovApiData[i];
+        for (var i in velovApiData) {
+            var velovData = velovApiData[i];
 
-        speech += 'La station ' + velovData.name;
+            speech += 'La station ' + velovData.name;
 
-        if (velovData.address != '')
-            speech += '... situé ' + velovData.address;
+            if (velovData.address != '')
+                speech += '... situé ' + velovData.address;
 
-        if (velovData.status === 'OPEN') {
-            speech += '... est ouverte. ...';
-        } else {
-            speech += '... est fermée. ...';
-            continue;
+            if (velovData.status === 'OPEN') {
+                speech += '... est ouverte. ...';
+            } else {
+                speech += '... est fermée. ...';
+                continue;
+            }
+
+            speech += conjugate(velovData.available_bikes,
+                ' {nb} vélos sont disponibles et ',
+                ' un vélo est disponible et ',
+                ' aucun vélo n\'est disponible et ');
+
+            speech += conjugate(velovData.available_bike_stands,
+                ' {nb} places sont libres.     ',
+                ' une place est libre.     ',
+                ' aucune place n\'est libre.     ');
         }
 
-        speech += velovData.available_bikes
-        + ' vélos sont disponibles et '
-        + velovData.available_bike_stands
-        + ' places sont libres.     ';
-    }
+        return speech;
+    },
 
-    return speech;
-};
+    /**
+     * Checks if Google correctly understanding the dictation
+     * @param search
+     * @param regex
+     */
+    checkGoogleUnderstanding = function(search, regex) {
+        var match = search.match(regex);
 
-/**
- * Aggregates the several Velov API returned data
- * @param nearestStationNumbers
- * @param index
- * @param velovApiData
- * @param callback
- */
-var aggregateVelovApiData = function (nearestStationNumbers, index, velovApiData, callback) {
-    callAPI(VELOV_URL + nearestStationNumbers[index].stationNumber
-    + '?contract=' + config.city
-    + '&apiKey=' + config.jcDecauxApiKey, function (nearestStation) {
-        velovApiData.push(nearestStation);
-        index++;
+        if (!match || match.length <= 1)
+            return false;
 
-        if (index < nearestStationNumbers.length) {
-            aggregateVelovApiData(nearestStationNumbers, index, velovApiData, callback)
-        } else {
-            callback(velovApiData);
-        }
-    });
-};
+        return match;
+    },
 
-/**
- * Calls API and returns the response in JSON format
- * @param url
- * @param callback
- */
-var callAPI = function (url, callback) {
-    request({'uri': url}, function (error, response, body) {
+    /**
+     * Aggregates the several Velov API returned data
+     * @param nearestStationNumbers
+     * @param index
+     * @param velovApiData
+     * @param callback
+     */
+    aggregateVelovApiData = function (nearestStationNumbers, index, velovApiData, callback) {
+        callAPI(VELOV_URL + nearestStationNumbers[index].stationNumber
+        + '?contract=' + properties.city
+        + '&apiKey=' + properties.jcDecauxApiKey, function (nearestStation) {
+            velovApiData.push(nearestStation);
+            index++;
 
-        if (error || response.statusCode != 200) {
-            callback({'tts': "Impossible d'accéder à l'API."});
-            return;
-        }
-
-        var apiResult = JSON.parse(body);
-        callback(apiResult);
-    });
-};
-
-/**
- * Computes distances between the Velov station and address geocoded latitude/longitude values
- * @param velovApiResult
- * @param geocodingPosition
- */
-var computeGeopositionDistances = function (velovApiResult, geocodingPosition) {
-    var stationDistances = [];
-    for (var i in velovApiResult) {
-        var stationVelov = velovApiResult[i];
-        stationDistances.push({
-            stationNumber: stationVelov.number,
-            distance: 6371 * Math.acos(Math.cos(radians(geocodingPosition.lat))
-            * Math.cos(radians(stationVelov.position.lat))
-            * Math.cos(radians(stationVelov.position.lng) -  radians(geocodingPosition.lng))
-            + Math.sin(radians(geocodingPosition.lat))
-            * Math.sin(radians(stationVelov.position.lat)))
+            if (index < nearestStationNumbers.length)
+                aggregateVelovApiData(nearestStationNumbers, index, velovApiData, callback)
+            else
+                callback(velovApiData);
         });
-    }
+    },
 
-    return stationDistances;
-};
+    /**
+     * Calls API and returns the response in JSON format
+     * @param url
+     * @param callback
+     */
+    callAPI = function (url, callback) {
+        request({'uri': url}, function (error, response, body) {
 
-/**
- * Searching of the nearest Velov station compared to the address (min distance)
- * @param stationDistances
- * @param nbStation
- */
-var findNearestVelovStations = function (stationDistances, nbStation) {
-    stationDistances.sort(function (a, b) {
-        return parseFloat(a.distance) - parseFloat(b.distance);
-    });
+            if (error || response.statusCode != 200)
+                return callback({'tts': "Impossible d'accéder à l'API."});
 
-    return stationDistances.slice(0, nbStation);
-};
+            var apiResult = JSON.parse(body);
+            callback(apiResult);
+        });
+    },
 
-/**
- * Writes a json file with all Velov station informations
- */
-var updateVelovStationList = function () {
-    // Call the JCDecaux Velov API to retrieve stations list
-    callAPI(VELOV_URL + stationNumberArg
-    + '?contract=' + config.city
-    + '&apiKey=' + config.jcDecauxApiKey, function (velovApiResult) {
-        jsonfile.writeFile(VELOV_LIST_FILENAME, velovApiResult);
-        console.log('Velov stations list file updated');
-    });
-};
+    /**
+     * Computes distances between the Velov station and address geocoded latitude/longitude values
+     * @param velovApiResult
+     * @param geocodingPosition
+     */
+    computeGeopositionDistances = function (velovApiResult, geocodingPosition) {
+        var stationDistances = [];
+        for (var i in velovApiResult) {
+            var stationVelov = velovApiResult[i];
+            stationDistances.push({
+                stationNumber: stationVelov.number,
+                distance: 6371 * Math.acos(Math.cos(radians(geocodingPosition.lat))
+                * Math.cos(radians(stationVelov.position.lat))
+                * Math.cos(radians(stationVelov.position.lng) -  radians(geocodingPosition.lng))
+                + Math.sin(radians(geocodingPosition.lat))
+                * Math.sin(radians(stationVelov.position.lat)))
+            });
+        }
 
-/**
- * Formats the address to pass it in url
- * @param address
- * @returns {string}
- */
-var formatAddress = function (address) {
-    return address.replace(/\ /g, '+')
-        .replace(/[èéêë]/gi, 'e')
-        .replace(/[àâä]/gi, 'a')
-        .replace(/[oôö]/gi, 'o')
-        .replace(/[ùûü]/gi, 'u')
-        .replace(/[iîï]/gi, 'i');
-};
+        return stationDistances;
+    },
 
-/**
- * Converts degrees to radians
- * @param degrees
- * @returns {number}
- */
-var radians = function (degrees) {
-    return degrees * Math.PI / 180;
-};
+    /**
+     * Searching of the nearest Velov station compared to the address (min distance)
+     * @param stationDistances
+     * @param nbStation
+     */
+    findNearestVelovStations = function (stationDistances, nbStation) {
+        stationDistances.sort(function (a, b) {
+            return parseFloat(a.distance) - parseFloat(b.distance);
+        });
+
+        return stationDistances.slice(0, nbStation);
+    },
+
+    /**
+     * Writes a json file with all Velov station informations
+     * @param callback
+     */
+    updateVelovStationList = function (callback) {
+        // Call the JCDecaux Velov API to retrieve stations list
+        callAPI(VELOV_URL
+        + '?contract=' + properties.city
+        + '&apiKey=' + properties.jcDecauxApiKey, function (velovApiResult) {
+            jsonfile.writeFile(__dirname + '\\' + VELOV_LIST_FILENAME, velovApiResult);
+
+            console.log('Velov stations list file updated');
+            return callback({'tts': 'La liste des stations Vélove à bien été mise à jour'});
+        });
+    },
+
+    /**
+     * Formats the address to pass it in url
+     * @param address
+     * @returns {string}
+     */
+    formatAddress = function (address) {
+        return address.replace(/\ /g, '+')
+            .replace(/[èéêë]/gi, 'e')
+            .replace(/[àâä]/gi, 'a')
+            .replace(/[oôö]/gi, 'o')
+            .replace(/[ùûü]/gi, 'u')
+            .replace(/[iîï]/gi, 'i');
+    },
+
+    /**
+     * Converts degrees to radians
+     * @param degrees
+     * @returns {number}
+     */
+    radians = function (degrees) {
+        return degrees * Math.PI / 180;
+    },
+
+    /**
+     * Change sentence according to the "nb" parameter
+     * @param nb
+     * @param several
+     * @param one
+     * @param none
+     * @returns {string}
+     */
+    conjugate = function (nb, several, one, none) {
+        var sentence = '';
+
+        if (nb > 1)
+            sentence += several;
+        else if (nb = 1)
+            sentence += one;
+        else
+            sentence += none;
+
+        return sentence.replace('{nb}', nb);
+    };
